9758621e1f60f913f073ef1ef3e16a8fe792efc3
sharmaayush3484 <143885201+sharmaayush3484@users.noreply.github.com>
2024-02-01 13:12:34 +0530

    added fun activity

---
+++ script.js
@@ -0,0 +1,41 @@
+var rect = document.querySelector(".center");
+
+rect.addEventListener("mousemove", function (details) {
+    var rectanglelocation = rect.getBoundingClientRect();
+    var insider = details.clientX - rectanglelocation.left;
+
+    if (insider < rectanglelocation.width / 2) {
+        var redcolor = gsap.utils.mapRange(
+            0,
+            rectanglelocation.width / 2,
+            255,
+            0,
+            insider
+        );
+        gsap.to(rect, {
+            backgroundColor: `rgb(${redcolor},0,0)`,
+            ease: Power4,
+        });
+    }
+    else {
+        var bluecolor = gsap.utils.mapRange(
+            rectanglelocation.width / 2,
+            rectanglelocation.width,
+            0,
+            255,
+            insider
+        );
+        gsap.to(rect, {
+            backgroundColor: `rgb(0,0,${bluecolor})`,
+            ease: Power4,
+        });
+    }
+});
+
+rect.addEventListener("mouseleave", function () {
+    gsap.to(rect, {
+        backgroundColor: "white",
+    })
+});
+
+
